cca51850225e7d446daedc0de289657d5d873196
jinarma <jinarma@gmail.com>
2022-06-24 11:10:16 +0530

    changed SECRET_KEY, installed decouple, created .env

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -8,3 +8,9 @@ def say_hello(request):
 	# transform
 	# send email, etc
 	return HttpResponse('Hello world')
+
+def show_sum(request):
+	sum_till_now = 0
+	for i in range(1, 101):
+		sum_till_now += i
+		print(f'{i} - {sum_till_now}')
